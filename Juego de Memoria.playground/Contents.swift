5745fcc7af248f4d0e074a5305541b7f6bdbab4b
José Luis de la Torre Pulido <Jose@imac-de-jose.local>
2016-04-23 10:29:09 +0200

    Añadí comentarios

--- Juego de Memoria.playground/Contents.swift
+++ Juego de Memoria.playground/Contents.swift
@@ -1,19 +1,25 @@
-//: Playground - noun: a place where people can play
+// Juego de Memoria
 
 import UIKit
 
+// creo el rango
 var numeros = 1...100
 
+// lo recorro
 for i in numeros {
+    // Si es divisible entre 5
     if i % 5 == 0 {
         print("#\(i)\tBingo!!!")
     }
+    // Si es par (divisible entre 2)
     if i % 2 == 0 {
         print("#\(i)\tPar!!!")
 
     }else {
+        // Si es impar
         print("#\(i)\tInpar!!!")
     }
+    // Si está entre 30 y 40 inclusive
     if i >= 30 && i <= 40 {
         print("#\(i)\tViva Swift!!!")
     }
